208fc7a68c6d2c87b97c2fab897132bbda1638e5
MNoman431 <nomanrehann112233@gmail.com>
2025-08-28 15:05:52 +0500

    clean code for auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,3 @@
-// import jwt from "jsonwebtoken";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// export const authenticate = (req, res, next) => {
-//   const token = req.headers.authorization?.split(" ")[1];
-//   console.log("yerha token ", token)
-//   if (!token) return res.status(401).json({ message: "Unauthorized" });
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-//     req.user = decoded;
-//     next();
-//   } catch (err) {
-//     res.status(401).json({ message: "Invalid token" });
-//   }
-// };
-
-
 
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
